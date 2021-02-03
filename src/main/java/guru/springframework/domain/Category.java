7d9873c59bb28db070f94a3207097ea9eeacbaca
Frank Jobst <fajobst@gmail.com>
2021-02-02 18:57:16 -0600

    added the many to many relationship between recipe and category

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,41 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String discription;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
