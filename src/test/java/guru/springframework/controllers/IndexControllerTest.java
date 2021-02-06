5509a60f6ccf923fbaf1b2b7efe79c4f2794688e
Frank Jobst <fajobst@gmail.com>
2021-02-06 16:11:09 -0600

    Added mockito tests for IndexController

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    Model model;
+
+
+    IndexController indexController;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+
+    }
+
+    @Test
+    public void getIndexPage()  throws  Exception {
+
+        String viewName = indexController.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+
+    }
+}
